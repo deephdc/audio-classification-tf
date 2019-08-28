f1a3e60731723d9c2305f5961bad58c9eb751fdb
ignacio <ignacio.heredia.cacha@gmail.com>
2019-08-28 18:25:42 +0200

    fix

--- audioclas/paths.py
+++ audioclas/paths.py
@@ -10,7 +10,7 @@ Github: ignacioheredia
 import os.path
 from datetime import datetime
 
-from imgclas import config
+from audioclas import config
 
 
 homedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
